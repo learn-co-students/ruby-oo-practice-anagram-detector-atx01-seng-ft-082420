476da4126cfadb90f9452f50db63ae66aecc26bb
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-09-01 10:05:41 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+
+    attr_accessor :word, :list
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match(list)
+        list.select do |annagram|
+            annagram.split("").sort == @word.split("").sort
+        end
+    end
+
+end
